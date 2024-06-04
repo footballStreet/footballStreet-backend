5eb40622275e816f37e8b05436d7fb7eacf60da9
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-04 23:11:13 +0300

    [FEATURE] add build app file

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import session from 'express-session';
+import methodOverride from 'method-override';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+export default app;
