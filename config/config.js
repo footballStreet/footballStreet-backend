5248836711f7d4824436c2a9de10099dd6694c13
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-08 16:55:46 +0300

    [FEATURE] add config setting

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export default {
+  "development": {
+    "username": "postgres",
+    "password": "qwerty",
+    "database": "interview_simulator_development",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "postgres",
+    "password": "qwerty",
+    "database": "interview_simulator",
+    "host": "localhost",
+    "dialect": "postgres"
+  }
+}
