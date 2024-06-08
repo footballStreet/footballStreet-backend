3242a33371331e226a57241c145b6978dc40c450
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-08 16:50:08 +0300

    [FEATURE] add user model

---
+++ models/user.js
@@ -0,0 +1,77 @@
+import { DataTypes, Model } from 'sequelize';
+import db from './index.js';
+
+class User extends Model {};
+
+const model = User.init(
+    {
+       id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+       },
+       username: {
+        type: DataTypes.TEXT,
+        unique: true,
+        validate: {
+            len: [6, 25],
+            notEmpty: true,
+        }
+       },
+       fist_name: {
+        type: DataTypes.TEXT,
+        validate: {
+            len: [1, 25],
+            notEmpty: true
+        }
+       },
+       last_name: {
+        type: DataTypes.TEXT,
+        validate: {
+            len: [1, 25],
+            notEmpty: true
+        }
+       },
+       surname: {
+        type: DataTypes.TEXT,
+        validate: {
+            len: [1, 25],
+            notEmpty: true
+        }
+       },
+       birthday: {
+        type: DataTypes.DATEONLY,
+        validate: {
+            notEmpty: true
+        }
+       },
+       phone: {
+        type: DataTypes.STRING,
+        validate: {
+            is: /^\+[0-9]{11}$/,
+            notEmpty: true
+        }
+       },
+       email: {
+        type: DataTypes.TEXT,
+        unique: true,
+        validate: {
+            notEmpty: null,
+            isEmail: true
+        }
+       },
+       password: {
+        type: DataTypes.TEXT,
+        validate: {
+            notEmpty: true,
+            min: 8
+        }
+       }
+    },
+    {
+        sequelize: db,
+        tableName: 'User',
+    }
+);
+
+export default model;
