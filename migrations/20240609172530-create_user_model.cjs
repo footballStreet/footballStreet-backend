1ab3dec21a6ddde7b59695a8c75656b2e3a16b75
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-09 21:02:43 +0300

    [FEATURE] add migration create_user_model

---
+++ migrations/20240609172530-create_user_model.cjs
@@ -0,0 +1,75 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('User', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+       },
+       username: {
+        type: Sequelize.DataTypes.TEXT,
+        unique: true,
+        validate: {
+            len: [6, 25],
+            notEmpty: true,
+        }
+       },
+       fist_name: {
+        type: Sequelize.DataTypes.TEXT,
+        validate: {
+            len: [1, 25],
+            notEmpty: true
+        }
+       },
+       last_name: {
+        type: Sequelize.DataTypes.TEXT,
+        validate: {
+            len: [1, 25],
+            notEmpty: true
+        }
+       },
+       surname: {
+        type: Sequelize.DataTypes.TEXT,
+        validate: {
+            len: [1, 25],
+            notEmpty: true
+        }
+       },
+       birthday: {
+        type: Sequelize.DataTypes.DATEONLY,
+        validate: {
+            notEmpty: true
+        }
+       },
+       phone: {
+        type: Sequelize.DataTypes.STRING,
+        validate: {
+            is: /^\+[0-9]{11}$/,
+            notEmpty: true
+        }
+       },
+       email: {
+        type: Sequelize.DataTypes.TEXT,
+        unique: true,
+        validate: {
+            notEmpty: null,
+            isEmail: true
+        }
+       },
+       password: {
+        type: Sequelize.DataTypes.TEXT,
+        validate: {
+            notEmpty: true,
+            min: 8
+        }
+       }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('User');
+  }
+};
