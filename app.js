b5672833674a410dd6ada357a69f148b40afe101
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-09 23:42:12 +0300

    [FEATURE] add users search router

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ import session from 'express-session';
 import methodOverride from 'method-override';
 import bodyParser from 'body-parser';
 import db from './models/index.js';
+import userRouter from './routes/user.router.js';
 
 db.authenticate()
   .catch(error => console.error(error))
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/users', userRouter)
 
 export default app;
