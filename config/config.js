48111cc2a8a37667e34c13649892df6353eff6f3
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-09 12:18:18 +0300

    [FEATURE] change database name

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ export default {
   "development": {
     "username": "postgres",
     "password": "qwerty",
-    "database": "interview_simulator_development",
+    "database": "interview_simulator_dev",
     "host": "localhost",
     "dialect": "postgres"
   },
   "production": {
     "username": "postgres",
     "password": "qwerty",
-    "database": "interview_simulator",
+    "database": "interview_simulator_prod",
     "host": "localhost",
     "dialect": "postgres"
   }
