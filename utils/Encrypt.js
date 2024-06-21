7b39b967a3c61d579d625998ae0b066501ac9966
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-21 20:18:47 +0300

    [FEATURE] add encrypt for hash password

---
+++ utils/Encrypt.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+
+class Encrypt {
+    constructor() {
+        this.saltRounds = 10;
+    };
+
+    hash(password) {
+        return bcrypt.hashSync(password, this.saltRounds);
+    };
+
+    compare(testPassword, hash) {
+        return bcrypt.compareSync(testPassword, hash) 
+    }
+
+};
+
+export default Encrypt;
