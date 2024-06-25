9e932dc849dd5ccc65ebff19eb78c35c766ababf
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-25 14:17:23 +0300

    [FEATURE] create migration for create games models

---
+++ migrations/20240625093427-games-model.cjs
@@ -0,0 +1,64 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('Games', {
+      id:{
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      city: {
+        type: Sequelize.DataTypes.STRING,
+        validate: {
+            len: [1, 50], 
+            notEmpty: true
+        }
+      },
+      street: {
+        type: Sequelize.DataTypes.STRING,
+        validate: {
+            len: [1, 50], 
+            notEmpty: true
+        }
+      }, 
+      build: {
+        type: Sequelize.DataTypes.INTEGER,
+        validate: {
+            min: 1
+        }
+      },
+      comment: {
+        type: Sequelize.DataTypes.TEXT,
+        validate: {
+            len: [0, 350]
+        }
+      },
+      starttime: {
+        type: Sequelize.DataTypes.DATE,
+      },
+      length: {
+        type: Sequelize.DataTypes.INTEGER, 
+        validate: {
+            min: 1, 
+            max: 6
+        }
+      },
+      userId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Games');
+  }
+};
