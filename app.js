30b6a6c78f6624672a8d246752225a3488fb6ca5
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-26 23:31:17 +0300

    [FIXTURE] add games router

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import methodOverride from 'method-override';
 import bodyParser from 'body-parser';
 import db from './models/index.js';
 import userRouter from './routes/user.router.js';
+import gamesRouter from './routes/games.router.js';
 
 db.authenticate()
   .catch(error => console.error(error))
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/users', userRouter)
+app.use('/users', userRouter);
+app.use('/games', gamesRouter);
 
 export default app;
