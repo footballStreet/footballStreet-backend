e25b4340dd10e736fc4161814291dcaed02a5d73
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-27 00:27:18 +0300

    [FEATURE] add router for return games list

--- routes/games.router.js
+++ routes/games.router.js
@@ -5,7 +5,12 @@ import GamesController from '../controllers/games.controller.js';
 const router = new Express();
 
 router.get('/', async (req, res) => {
-    // => get all list users
+    try {
+        const games = await new GamesController().getGames(req, res);
+        res.status(200).send(JSON.stringify(games, null, 2));
+    } catch (error) {
+        res.status(503).send(`Сервер временно недоступен и не может отработать текущий запрос: ${error}`)
+    }
 });
 
 router.post('/', async (req, res) => {
