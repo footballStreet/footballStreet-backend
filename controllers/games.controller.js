78b38006b59f5f4f20bb204659cfd523fa47b84f
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-26 23:32:21 +0300

    [FIXTURE] add controller for create game

---
+++ controllers/games.controller.js
@@ -0,0 +1,22 @@
+import gamesModel from '../models/games.js';
+
+class GamesController{
+    async getGames(__req, __res) {
+        const games = await gamesModel.findAll({raw: true});
+        return games;
+    };
+
+    async createGames(req, __res) {
+        const { city, street, build, comment, year, day, month, hour, minutes, length } = req.body;
+        // const userId = req.params.id; After add session
+        const userId = 1;
+        const startTime = new Date(year, day, month, hour, minutes);
+        const newGame = await gamesModel.create(
+            { city, street, build, comment, starttime: startTime, length, userId }, 
+            { fields: ['city', 'street', 'build', 'comment', 'starttime', 'length', 'userId'] }
+        );
+        return newGame;
+    }
+};
+
+export default GamesController;
