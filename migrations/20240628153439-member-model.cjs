ec8061e31cfd1b7cac6e9f7b321db0a19e49ba6f
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-28 18:43:29 +0300

    [FEATURE] create migration for create Member model

---
+++ migrations/20240628153439-member-model.cjs
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('Member', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      }, 
+      gamesId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Game',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, 
+      userId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'User',            
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, 
+      creator: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        validate: {
+          notEmpty: true
+        }
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Member');
+  }
+};
