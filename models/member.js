24c6186a1051234e4fe6b759e0cabdd06d1e42a8
nikitakozlovjr <nikita3726898@gmail.com>
2024-06-28 18:42:50 +0300

    [FEATURE] add new Memeber model for keep info for member game

---
+++ models/member.js
@@ -0,0 +1,48 @@
+import { DataTypes, Model } from 'sequelize';
+import db from './index.js';
+
+class Member extends Model {};
+
+const model = Member.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        }, 
+        gamesId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'Game',
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        }, 
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'User',
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        }, 
+        creator: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                notEmpty: true
+            }
+        }
+    }, 
+    {
+        sequelize: db,
+        tableName: 'Member',
+        timestamps: false
+    }
+);
+
+
+export default model;
